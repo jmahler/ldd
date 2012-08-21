294318463b1abc96aafd08438de43da0614c4a41
Jeremiah Mahler <jmahler@mail.csuchico.edu>
2012-08-21 15:56:28 -0700

    added example which takes a parameter

---
+++ param/hello.c
@@ -0,0 +1,37 @@
+
+/*
+ * Shows how a paramter can be given to insmod.
+ *
+ * # insmod hello.ko howmany=3
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/stat.h>
+
+static int howmany = 1;
+module_param(howmany, int, S_IRUGO);
+// This will also create an entry in /sys/module/hello/parameters/howmany
+
+static int __init hello_init(void)
+{
+	int i;
+
+	for (i = 0; i < howmany; i++) {
+		pr_info("Hello world.\n");
+	}
+
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("Goodbye world.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
