ac1477698878b1419592f5e8ef9b9fb1d1e37d18
Jeremiah Mahler <jmmahler@gmail.com>
2013-07-25 23:26:29 -0700

    merged changes between hello and param modules

--- hello/hello.c
+++ hello/hello.c
@@ -1,24 +1,19 @@
 
-/*
- * Prints a message on module insertion and another message on exit.
- *
- * This code is from Pg. 37 of Linux Device Drivers by Jonathan Corbet.
- */
-
 #include <linux/init.h>
 #include <linux/module.h>
-MODULE_LICENSE("Dual BSD/GPL");
 
-static int hello_init(void)
+static int __init hello_init(void)
 {
 	printk(KERN_ALERT "Hello, World\n");
 	return 0;
 }
 
-static void hello_exit(void)
+static void __exit hello_exit(void)
 {
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
--- param/hello.c
+++ param/hello.c
@@ -1,17 +1,7 @@
 
-/*
- * Shows how a paramter can be given to insmod.
- *
- * # insmod hello.ko howmany=3
- *
- * Messages will be sent to syslog.
- */
-
 #include <linux/init.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include <linux/stat.h>
 
 static int howmany = 1;
 module_param(howmany, int, S_IRUGO);
@@ -22,7 +12,7 @@ static int __init hello_init(void)
 	int i;
 
 	for (i = 0; i < howmany; i++) {
-		pr_info("Hello world.\n");
+		printk(KERN_ALERT "Hello, World\n");
 	}
 
 	return 0;
@@ -33,7 +23,7 @@ static void __exit hello_exit(void)
 	int i;
 
 	for (i = 0; i < howmany; i++) {
-		pr_info("Goodbye world.\n");
+		printk(KERN_ALERT "Goodbye, cruel world\n");
 	}
 }
 
