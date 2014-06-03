cac73771ced0f1dc8b578bb3e074f70ed1594923
Jeremiah Mahler <jmmahler@gmail.com>
2014-06-02 21:13:46 -0700

    usb keyboard class, simplified message
    The hello-usb example has be changed so that it class based and only
    activates for a keyboard class.
    
    Shortened the messages on connect/disconnect, since there are only two
    now.

--- hello-usb/hello.c
+++ hello-usb/hello.c
@@ -2,12 +2,11 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/usb.h>
+#include <linux/hid.h>
 
 static const struct usb_device_id hello_table[] = {
-	/*{.driver_info = 42}, */ /* call for every usb device */
-	{ USB_DEVICE(0x046e, 0x6000) },
-	{ USB_DEVICE(0x04f3, 0x0801) },
-	{ USB_DEVICE(0x0403, 0xffa8) },
+	{ USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID, USB_INTERFACE_SUBCLASS_BOOT,
+			USB_INTERFACE_PROTOCOL_KEYBOARD) },
 	{ }
 };
 MODULE_DEVICE_TABLE(usb, hello_table);
@@ -15,14 +14,14 @@ MODULE_DEVICE_TABLE(usb, hello_table);
 static int hello_probe(struct usb_interface *intf,
 						const struct usb_device_id *id)
 {
-	pr_debug("usb probe: Hello, World\n");
+	pr_debug("Hello, World\n");
 
 	return 0;  /* yes, that's our device */
 }
 
 static void hello_disconnect(struct usb_interface *intf)
 {
-	pr_debug("usb disconnect: Goodbye, cruel world\n");
+	pr_debug("Goodbye, cruel world\n");
 }
 
 static struct usb_driver hello_driver = {
