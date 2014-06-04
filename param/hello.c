79beadf6138cfea524f379f5f76ba29181f57e26
Jeremiah Mahler <jmmahler@gmail.com>
2014-06-04 06:55:58 -0700

    param/hello.c: Fixed checkpatch.pl warnings
    Fixed checkpatch.pl warnings:
    
      - braces not necessary for single statement blocks

--- param/hello.c
+++ param/hello.c
@@ -1,3 +1,4 @@
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -9,9 +10,8 @@ static int __init hello_init(void)
 {
 	int i;
 
-	for (i = 0; i < howmany; i++) {
+	for (i = 0; i < howmany; i++)
 		pr_alert("Hello, World\n");
-	}
 
 	return 0;
 }
@@ -20,13 +20,13 @@ static void __exit hello_exit(void)
 {
 	int i;
 
-	for (i = 0; i < howmany; i++) {
+	for (i = 0; i < howmany; i++)
 		pr_alert("Goodbye, cruel world\n");
-	}
 }
+
+module_init(hello_init);
+module_exit(hello_exit);
 
 MODULE_AUTHOR("Jeremiah Mahler <jmmahler@gmail.com>");
 MODULE_LICENSE("GPL");
 
-module_init(hello_init);
-module_exit(hello_exit);
