2bfc9a4339bde367dfef84fb94669a01a67fc0ed
Jeremiah Mahler <jmmahler@gmail.com>
2014-06-03 22:25:30 -0700

    data_ioctl/data.c: Fixed checkpatch.pl warnings
    Fixed checkpatch.pl warnings:
    
      - pr_warn instead of printk(KERN_WARNING
    
      - over long line with 8 space tabs
    
      - switch statement indentation
    
      - // C99 comments
    
      - const struct file_operations
    
      - missing space after ','

--- data_ioctl/data.c
+++ data_ioctl/data.c
@@ -1,3 +1,4 @@
+
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/fs.h>
@@ -31,7 +32,7 @@ static int data_open(struct inode *inode, struct file *filp)
 
 #define DATA_IOC_MAGIC 'm'
 
-#define DATA_IOCRESET _IO(DATA_IOC_MAGIC,  1)
+#define DATA_IOCRESET 	_IO(DATA_IOC_MAGIC,  1)
 #define DATA_IOCWX	_IOW(DATA_IOC_MAGIC, 2, int)
 #define DATA_IOCRX	_IOR(DATA_IOC_MAGIC, 3, int)
 
@@ -43,41 +44,43 @@ static long data_ioctl(struct file *filp, unsigned int cmd,
 	int err = 0;
 	int retval = 0;
 
-	//struct data_dev *data_dev = filp->private_data;
+	/*struct data_dev *data_dev = filp->private_data; */
 
 	if (_IOC_TYPE(cmd) != DATA_IOC_MAGIC) {
-		printk(KERN_ALERT "invalid ioctl magic\n");
+		pr_alert("invalid ioctl magic\n");
 		return -ENOTTY;
 	}
 	if (_IOC_NR(cmd) > DATA_IOC_MAXNR) {
-		printk(KERN_ALERT "ioctl beyond maximum\n");
+		pr_alert("ioctl beyond maximum\n");
 		return -ENOTTY;
 	}
 
 	if (_IOC_DIR(cmd) & _IOC_READ)
-		err = !access_ok(VERIFY_WRITE, (void __user *) arg, _IOC_SIZE(cmd));
+		err = !access_ok(VERIFY_WRITE, (void __user *) arg,
+					_IOC_SIZE(cmd));
 	else if (_IOC_DIR(cmd) & _IOC_WRITE)
-		err = !access_ok(VERIFY_READ, (void __user *) arg, _IOC_SIZE(cmd));
+		err = !access_ok(VERIFY_READ, (void __user *) arg,
+					_IOC_SIZE(cmd));
 	if (err)
 		return -EFAULT;
 
 	switch (cmd) {
-		case DATA_IOCRESET:
-			/* takes no argument, sets values to default */
-			x = 0;
-			break;
-		case DATA_IOCRX:
-			/* read integer */
-			retval = __put_user(x, (int __user *) arg);
-			break;
-		case DATA_IOCWX:
-			/* write integer */
-			retval = __get_user(x, (int __user *) arg);
-			break;
-		default:
-			return -ENOTTY;  /* POSIX standard */
-			//return -EINVAL;  /* common */
-			/* Pg. 161 Linux Device Drivers (2005) */
+	case DATA_IOCRESET:
+		/* takes no argument, sets values to default */
+		x = 0;
+		break;
+	case DATA_IOCRX:
+		/* read integer */
+		retval = __put_user(x, (int __user *) arg);
+		break;
+	case DATA_IOCWX:
+		/* write integer */
+		retval = __get_user(x, (int __user *) arg);
+		break;
+	default:
+		return -ENOTTY;  /* POSIX standard */
+		/*return -EINVAL; */  /* common */
+		/* Pg. 161 Linux Device Drivers (2005) */
 	}
 
 	return retval;
@@ -88,7 +91,7 @@ int data_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-struct file_operations data_fops = {
+const struct file_operations data_fops = {
 	.owner = THIS_MODULE,
 	.open = data_open,
 	.unlocked_ioctl = data_ioctl,
@@ -103,7 +106,7 @@ static int __init data_init(void)
 
 	err = alloc_chrdev_region(&data_major, 0, 1, DEVICE_NAME);
 	if (err < 0) {
-		printk(KERN_WARNING "Unable to register device\n");
+		pr_warn("Unable to register device\n");
 		goto err_chrdev_region;
 	}
 
@@ -111,7 +114,7 @@ static int __init data_init(void)
 
 	data_devp = kmalloc(sizeof(struct data_dev), GFP_KERNEL);
 	if (!data_devp) {
-		printk(KERN_WARNING "Unable to kmalloc data_devp\n");
+		pr_warn("Unable to kmalloc data_devp\n");
 		err = -ENOMEM;
 		goto err_malloc_data_devp;
 	}
@@ -120,14 +123,15 @@ static int __init data_init(void)
 	data_devp->cdev.owner = THIS_MODULE;
 	err = cdev_add(&data_devp->cdev, data_major, 1);
 	if (err) {
-		printk(KERN_WARNING "cdev_add failed\n");
+		pr_warn("cdev_add failed\n");
 		goto err_cdev_add;
 	}
 
 	data_device = device_create(data_class, NULL,
-							MKDEV(MAJOR(data_major), 0), NULL, "data%d",0);
+			MKDEV(MAJOR(data_major), 0), NULL, "data%d", 0);
+
 	if (IS_ERR(data_device)) {
-		printk(KERN_WARNING "device_create failed\n");
+		pr_warn("device_create failed\n");
 		err = PTR_ERR(data_device);
 		goto err_device_create;
 	}
@@ -159,8 +163,9 @@ static void __exit data_exit(void)
 	unregister_chrdev_region(data_major, 1);
 }
 
-MODULE_AUTHOR("Jeremiah Mahler <jmmahler@gmail.com>");
-MODULE_LICENSE("GPL");
-
 module_init(data_init);
 module_exit(data_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Jeremiah Mahler <jmmahler@gmail.com>");
+
