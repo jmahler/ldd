2f97dd318e7661b6c1fbcc94d4c0f9e70ea8618c
Jeremiah Mahler <jmmahler@gmail.com>
2014-06-03 22:55:54 -0700

    fifo_fix/fifo.c: Fixed checkpatch.pl warnings
    Fixed checkpatch.pl warnings:
    
      - // C99 comments
    
      - braces not necessary for single statement blocks
    
      - do not initialize statics to zero
    
      - { on next line for function definitions
    
      - const struct file_operations
    
      - over long line with 8 space tabs
    
      - missing space after ','
    
      - "foo* bar" should be "foo *bar"

--- fifo_fix/fifo.c
+++ fifo_fix/fifo.c
@@ -1,3 +1,4 @@
+
 #include <linux/cdev.h>
 #include <linux/delay.h>
 #include <linux/device.h>
@@ -23,10 +24,10 @@ struct fifo_dev {
 	char *fifo_start;
 	char *fifo_end;
 	int empty;
-	struct mutex ferw_mtx;  // ferw = fifo, empty, read, write
+	struct mutex ferw_mtx;  /* ferw = fifo, empty, read, write */
 } *fifo_devp;
 
-int fifo_open(struct inode* inode, struct file* filp)
+int fifo_open(struct inode *inode, struct file *filp)
 {
 	struct fifo_dev *fifo_devp;
 
@@ -37,40 +38,40 @@ int fifo_open(struct inode* inode, struct file* filp)
 	return 0;
 }
 
-#define DEFINE_PWAIT(uid)													\
-																			\
-static DEFINE_MUTEX(in_mtx_##uid);											\
-static DEFINE_MUTEX(out_mtx_##uid);											\
-																			\
-static void pwait_##uid(void) {												\
-	static int in = 0;														\
-	static int out = 0;														\
-																			\
-	mutex_lock(&in_mtx_##uid);												\
-	if (++in >= 2) {														\
-		mutex_lock(&out_mtx_##uid);											\
-		out += 2;															\
-		mutex_unlock(&out_mtx_##uid);										\
-		in -= 2;															\
-	}																		\
-	mutex_unlock(&in_mtx_##uid);											\
-																			\
-	do {																	\
-		mutex_lock(&out_mtx_##uid);											\
-		if (out) {															\
-			out -= 1;														\
-			mutex_unlock(&out_mtx_##uid);									\
-			break;															\
-		}																	\
-		mutex_unlock(&out_mtx_##uid);										\
-	} while (1);															\
+#define DEFINE_PWAIT(uid)						\
+									\
+static DEFINE_MUTEX(in_mtx_##uid);					\
+static DEFINE_MUTEX(out_mtx_##uid);					\
+									\
+static void pwait_##uid(void)						\
+{									\
+	static int in;							\
+	static int out;							\
+									\
+	mutex_lock(&in_mtx_##uid);					\
+	if (++in >= 2) {						\
+		mutex_lock(&out_mtx_##uid);				\
+		out += 2;						\
+		mutex_unlock(&out_mtx_##uid);				\
+		in -= 2;						\
+	}								\
+	mutex_unlock(&in_mtx_##uid);					\
+									\
+	do {								\
+		mutex_lock(&out_mtx_##uid);				\
+		if (out) {						\
+			out -= 1;					\
+			mutex_unlock(&out_mtx_##uid);			\
+			break;						\
+		}							\
+		mutex_unlock(&out_mtx_##uid);				\
+	} while (1);							\
 }
 
 DEFINE_PWAIT(fifo_read);
 
 static ssize_t fifo_read(struct file *filp, char __user *buf,
-							size_t count,
-							loff_t *f_pos)
+						size_t count, loff_t *f_pos)
 {
 	struct fifo_dev *dev = filp->private_data;
 	size_t left;
@@ -94,15 +95,14 @@ static ssize_t fifo_read(struct file *filp, char __user *buf,
 		}
 		left--;
 
-		if (dev->read_ptr == dev->fifo_end) {
+		if (dev->read_ptr == dev->fifo_end)
 			dev->read_ptr = dev->fifo_start;
-		} else {
+		else
 			(dev->read_ptr)++;
-		}
 
-		if (dev->read_ptr == dev->write_ptr) {
+		if (dev->read_ptr == dev->write_ptr)
 			dev->empty = 1;
-		}
+
 		mutex_unlock(&dev->ferw_mtx);
 	}
 
@@ -112,8 +112,7 @@ static ssize_t fifo_read(struct file *filp, char __user *buf,
 DEFINE_PWAIT(fifo_write);
 
 static ssize_t fifo_write(struct file *filp, const char __user *buf,
-							size_t count,
-							loff_t *f_pos)
+						size_t count, loff_t *f_pos)
 {
 	struct fifo_dev *dev = filp->private_data;
 	size_t left;
@@ -140,11 +139,10 @@ static ssize_t fifo_write(struct file *filp, const char __user *buf,
 		if (dev->empty)
 			dev->empty = 0;
 
-		if (dev->write_ptr == dev->fifo_end) {
+		if (dev->write_ptr == dev->fifo_end)
 			dev->write_ptr = dev->fifo_start;
-		} else {
+		else
 			(dev->write_ptr)++;
-		}
 		mutex_unlock(&dev->ferw_mtx);
 	}
 
@@ -156,7 +154,7 @@ static int fifo_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-struct file_operations fifo_fops = {
+const struct file_operations fifo_fops = {
 	.owner = THIS_MODULE,
 	.open = fifo_open,
 	.read = fifo_read,
@@ -166,39 +164,37 @@ struct file_operations fifo_fops = {
 
 
 static ssize_t read_offset_show(struct device *dev,
-								struct device_attribute *attr,
-								char *buf)
+				struct device_attribute *attr, char *buf)
 {
 	struct fifo_dev *fifo_devp = dev_get_drvdata(dev);
-	return sprintf(buf, "%u\n", (unsigned int) (fifo_devp->read_ptr
-													- fifo_devp->fifo_start));
+	return sprintf(buf, "%u\n",
+			(unsigned int) (fifo_devp->read_ptr -
+				fifo_devp->fifo_start));
 }
 
 static ssize_t read_offset_store(struct device *dev,
-									struct device_attribute *attr,
-									const char *buf,
-									size_t count)
+					struct device_attribute *attr,
+					const char *buf, size_t count)
 {
-	return 0;  // cannot store anything
+	return 0;  /* cannot store anything */
 }
 
 static DEVICE_ATTR(read_offset, 0666, read_offset_show, read_offset_store);
 
 static ssize_t write_offset_show(struct device *dev,
-								struct device_attribute *attr,
-								char *buf)
+				struct device_attribute *attr, char *buf)
 {
 	struct fifo_dev *fifo_devp = dev_get_drvdata(dev);
-	return sprintf(buf, "%u\n", (unsigned int) (fifo_devp->write_ptr
-													- fifo_devp->fifo_start));
+	return sprintf(buf, "%u\n", (unsigned int) (fifo_devp->write_ptr -
+					fifo_devp->fifo_start));
 }
 
 static ssize_t write_offset_store(struct device *dev,
-									struct device_attribute *attr,
-									const char *buf,
-									size_t count)
+					struct device_attribute *attr,
+					const char *buf,
+					size_t count)
 {
-	return 0;  // cannot store anything
+	return 0;  /* cannot store anything */
 }
 
 static DEVICE_ATTR(write_offset, 0666, write_offset_show, write_offset_store);
@@ -241,7 +237,7 @@ static int __init fifo_init(void)
 	}
 
 	fifo_device = device_create(fifo_class, NULL,
-							MKDEV(MAJOR(fifo_major), 0), NULL, "fifo%d",0);
+				MKDEV(MAJOR(fifo_major), 0), NULL, "fifo%d", 0);
 	if (IS_ERR(fifo_device)) {
 		pr_warn("device_create failed\n");
 		err = PTR_ERR(fifo_device);
@@ -249,24 +245,21 @@ static int __init fifo_init(void)
 	}
 
 	err = dev_set_drvdata(fifo_device, fifo_devp);
-	if (err) {
+	if (err)
 		goto err_set_drvdata;
-	}
 
 	err = device_create_file(fifo_device, &dev_attr_read_offset);
-	if (err) {
+	if (err)
 		goto err_file_read_offset;
-	}
 
 	err = device_create_file(fifo_device, &dev_attr_write_offset);
-	if (err) {
+	if (err)
 		goto err_file_write_offset;
-	}
 
 	return 0;  /* success */
 
 err_file_write_offset:
-	//device_remove_file(fifo_device, &dev_attr_write_offset);
+	/* device_remove_file(fifo_device, &dev_attr_write_offset); */
 err_file_read_offset:
 	device_remove_file(fifo_device, &dev_attr_read_offset);
 err_set_drvdata:
@@ -300,8 +293,9 @@ static void __exit fifo_exit(void)
 	unregister_chrdev_region(fifo_major, 1);
 }
 
-MODULE_AUTHOR("Jeremiah Mahler <jmmahler@gmail.com>");
-MODULE_LICENSE("GPL");
-
 module_init(fifo_init);
 module_exit(fifo_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Jeremiah Mahler <jmmahler@gmail.com>");
+
