eb9d9bb5d2990a3603dca481311e65b5405ed0a3
Jeremiah Mahler <jmmahler@gmail.com>
2014-06-04 07:21:08 -0700

    sysx_ktype/sysx.c: Fixed checkpatch.pl warnings
    Fixed checkpatch.pl warnings:
    
      - brackets not needed for single statement blocks
    
      - return value of sscanf not being checked
    
      - // no C99 comments
    
      - const struct sysfs_ops

--- sysx_ktype/sysx.c
+++ sysx_ktype/sysx.c
@@ -13,21 +13,24 @@ static int y;
 static ssize_t sysx_xy_show(struct kobject *kobj, struct attribute *attr,
 						char *buf)
 {
-	if (0 == strcmp(attr->name, "x")) {
+	if (0 == strcmp(attr->name, "x"))
 		return sprintf(buf, "%d\n", x);
-	} else {
+	else
 		return sprintf(buf, "%d\n", y);
-	}
 }
 
 static ssize_t sysx_xy_store(struct kobject *kobj, struct attribute *attr,
 						const char *buf, size_t count)
 {
-	if (0 == strcmp(attr->name, "x")) {
-		sscanf(buf, "%du", &x);
-	} else {
-		sscanf(buf, "%du", &y);
-	}
+	int ret;
+
+	if (0 == strcmp(attr->name, "x"))
+		ret = sscanf(buf, "%du", &x);
+	else
+		ret = sscanf(buf, "%du", &y);
+
+	if (ret != 1)
+		return -EINVAL;
 
 	return count;
 }
@@ -36,7 +39,7 @@ void sysx_xy_release(struct kobject *kobj)
 {
 }
 
-static struct sysfs_ops sysx_sysfs_ops = {
+static const struct sysfs_ops sysx_sysfs_ops = {
 	.show = sysx_xy_show,
 	.store = sysx_xy_store,
 };
@@ -59,7 +62,7 @@ struct attribute y_attr = {
 static struct attribute *attrs[] = {
 	&x_attr,
 	&y_attr,
-	NULL,  // terminate list
+	NULL,  /* terminate list */
 };
 
 static struct attribute_group attr_group = {
@@ -95,3 +98,4 @@ module_exit(sysx_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jeremiah Mahler <jmmahler@gmail.com>");
+
