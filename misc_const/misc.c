59965bd2069481fec137923a6852b18bc87ba557
Jeremiah Mahler <jmmahler@gmail.com>
2014-06-10 20:43:48 -0700

    misc: simplify using ARRAY_SIZE and get_user
    Eliminate the strlen_misc_id macro and replace with the commonly used
    ARRAY_SIZE macro.
    
    Change misc_write to use get_user and check character by character
    instead of using copy_from_user.  Eliminate the need for a buffer
    inside the function.

--- misc_const/misc.c
+++ misc_const/misc.c
@@ -9,30 +9,31 @@
 #define DEVICE_NAME "misc"
 
 const char misc_id[] = "aeda58c25c67";
-#define strlen_misc_id (sizeof(misc_id) - 1)  /* w/o '\0' */
 
 static ssize_t misc_read(struct file *filp, char __user *buf,
 				size_t count, loff_t *f_pos)
 {
 	return simple_read_from_buffer(buf, count, f_pos, misc_id,
-			strlen_misc_id);
+			ARRAY_SIZE(misc_id) - 1);
 }
 
 static ssize_t misc_write(struct file *filp, const char __user *buf,
 				size_t count, loff_t *f_pos)
 {
-	char kbuf[strlen_misc_id];
+	char c;
+	unsigned int i;
 
-	if (count != strlen_misc_id)
+	if (count != ARRAY_SIZE(misc_id) - 1)
 		return -EINVAL;
 
-	if (copy_from_user(kbuf, buf, count) != 0)
-		return -EIO;
+	for (i = 0; i < count; i++) {
+		if (get_user(c, buf + i))
+			return -EIO;
+		if (misc_id[i] != c)
+			return -EINVAL;
+	}
 
-	if (strncmp(misc_id, buf, count))
-		return -EINVAL;
-
-	return count;
+	return count;  /* success */
 }
 
 static const struct file_operations misc_fops = {
