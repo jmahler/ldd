4e89c7e08b363f9157c017c342d32df2c6bfacc2
Jeremiah Mahler <jmmahler@gmail.com>
2014-06-13 11:41:25 -0700

    hello-debugfs: cleanup
    Cleanup driver by eliminating unnecessary global variables, shortening
    names (no hello_ prefix), and other general improvements.

--- hello-debugfs/hello.c
+++ hello-debugfs/hello.c
@@ -2,138 +2,124 @@
 #include <linux/debugfs.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/slab.h>
 
 #define DEVICE_NAME "hello"
 
-const char hello_id[] = "aeda58c25c67";
+const char id[] = "aeda58c25c67";
+#define ID_SIZE (ARRAY_SIZE(id) - 1)
 
-struct hello_dev {
-	struct dentry *root;
-	struct dentry *id;
-	struct dentry *d_jiffies;
-	void *foo;
-} *hello_devp;
+void *foo_data;
+#define FOO_DATA_SIZE PAGE_SIZE
+DEFINE_MUTEX(foo_lock);
 
-static ssize_t hello_id_read(struct file *filp, char __user *buf,
-				size_t count, loff_t *f_pos)
+static struct dentry *root;
+
+static ssize_t id_read(struct file *filp, char __user *buf,
+					size_t count, loff_t *f_pos)
 {
-	return simple_read_from_buffer(buf, count, f_pos, hello_id,
-			ARRAY_SIZE(hello_id) - 1);
+	return simple_read_from_buffer(buf, count, f_pos, id, ID_SIZE);
 }
 
-static ssize_t hello_id_write(struct file *filp, const char __user *buf,
-				size_t count, loff_t *f_pos)
+/* return -EINVAL if incorrect id is written, return count if correct */
+static ssize_t id_write(struct file *filp, const char __user *buf,
+					size_t count, loff_t *f_pos)
 {
-	char kbuf[ARRAY_SIZE(hello_id) - 1];
+	char kbuf[ID_SIZE];
 
-	if (count != ARRAY_SIZE(hello_id) - 1)
+	if (count != ID_SIZE)
 		return -EINVAL;
 
 	if (!simple_write_to_buffer(kbuf, ARRAY_SIZE(kbuf), f_pos, buf, count))
 		return -EINVAL;
 
-	if (strncmp(hello_id, kbuf, ARRAY_SIZE(hello_id) - 1))
+	if (strncmp(id, kbuf, ID_SIZE))
 		return -EINVAL;
 
-	return count;  /* success */
+	return count;  /* correct */
 }
 
-static const struct file_operations hello_id_fops = {
+static const struct file_operations id_fops = {
 	.owner = THIS_MODULE,
-	.read = hello_id_read,
-	.write = hello_id_write,
+	.read  = id_read,
+	.write = id_write,
 };
 
-DEFINE_MUTEX(foo_lock);
-
-static ssize_t hello_foo_read(struct file *filp, char __user *buf,
+static ssize_t foo_read(struct file *filp, char __user *buf,
 				size_t count, loff_t *f_pos)
 {
 	int ret;
 
 	if (mutex_lock_interruptible(&foo_lock))
 		return -EINTR;
-	ret = simple_read_from_buffer(buf, count, f_pos, hello_devp->foo,
-			PAGE_SIZE);
+	ret = simple_read_from_buffer(buf, count, f_pos,
+					foo_data, FOO_DATA_SIZE);
 	mutex_unlock(&foo_lock);
 
 	return ret;
 }
 
-static ssize_t hello_foo_write(struct file *filp, const char __user *buf,
+static ssize_t foo_write(struct file *filp, const char __user *buf,
 				size_t count, loff_t *f_pos)
 {
 	int ret;
 
 	if (mutex_lock_interruptible(&foo_lock))
 		return -EINTR;
-	ret = simple_write_to_buffer(hello_devp->foo, PAGE_SIZE,
+	ret = simple_write_to_buffer(foo_data, FOO_DATA_SIZE,
 					f_pos, buf, count);
 	mutex_unlock(&foo_lock);
 	if (!ret)
 		return -EIO;
 
-	return count;  /* success */
+	return count;
 }
 
-static const struct file_operations hello_foo_fops = {
+static const struct file_operations foo_fops = {
 	.owner = THIS_MODULE,
-	.read = hello_foo_read,
-	.write = hello_foo_write,
+	.read  = foo_read,
+	.write = foo_write,
 };
 
 static int __init hello_init(void)
 {
-	struct dentry *dbg_foo;
+	struct dentry *id, *jif, *foo;
 
-	hello_devp = kmalloc(sizeof(struct hello_dev), GFP_KERNEL);
-	if (!hello_devp)
-		goto err_malloc_devp;
-
-	hello_devp->foo = kmalloc(PAGE_SIZE, GFP_KERNEL);
-	if (!hello_devp)
+	foo_data = kmalloc(FOO_DATA_SIZE, GFP_KERNEL);
+	if (!foo_data)
 		goto err_malloc_foo;
 
-	hello_devp->root = debugfs_create_dir(DEVICE_NAME, NULL);
-	if (!hello_devp->root)
+	root = debugfs_create_dir(DEVICE_NAME, NULL);
+	if (!root)
 		goto err_debugfs_root;
 
-	hello_devp->id = debugfs_create_file("id", 0666,
-					hello_devp->root, hello_devp,
-					&hello_id_fops);
-	if (!hello_devp->id)
+	id = debugfs_create_file("id", 0666, root, NULL, &id_fops);
+	if (!id)
 		goto err_debugfs_files;
 
-	hello_devp->d_jiffies = debugfs_create_u64("jiffies", 0444,
-					hello_devp->root,
-					(u64 *) &jiffies);
-	if (!hello_devp->d_jiffies)
+	jif = debugfs_create_u64("jiffies", 0444, root, (u64 *) &jiffies);
+	if (!jif)
 		goto err_debugfs_files;
 
-	dbg_foo = debugfs_create_file("foo", 0644,
-				hello_devp->root, NULL,
-				&hello_foo_fops);
-	if (!dbg_foo)
+	foo = debugfs_create_file("foo", 0644, root, NULL, &foo_fops);
+	if (!foo)
 		goto err_debugfs_files;
 
 	return 0;
 
 err_debugfs_files:
-	debugfs_remove_recursive(hello_devp->root);
+	debugfs_remove_recursive(root);
 err_debugfs_root:
-	kfree(hello_devp->foo);
+	kfree(foo_data);
 err_malloc_foo:
-	kfree(hello_devp);
-err_malloc_devp:
 	return -ENOMEM;
 }
 
 static void __exit hello_exit(void)
 {
-	debugfs_remove_recursive(hello_devp->root);
-	kfree(hello_devp->foo);
-	kfree(hello_devp);
+	debugfs_remove_recursive(root);
+	kfree(foo_data);
 }
 
 module_init(hello_init);
