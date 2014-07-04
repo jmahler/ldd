87a87a5df70433f8292c422c44c5be812b45d1f0
Jeremiah Mahler <jmmahler@gmail.com>
2014-07-04 07:09:05 -0700

    id_jiff_foo: include foo_debufs
    Place foo_debugfs under id_jiff_foo/ which was missed during previous
    re-organization.  Generalize its test script and move to the parent
    directory.

---
+++ id_jiff_foo/test-foo.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+	cat <<-EOF
+		  usage: $0 <device>
+		    <device> := "/sys/kernel/debug/foo_debugfs/foo" | ...
+	EOF
+	exit 1
+fi
+
+DEV=$1
+
+set -vx
+
+# store services in foo
+sudo dd if=/etc/services of=$DEV bs=4096 count=1
+grep ssh $DEV
+
