27d29983a76cd4f79ca6a5a565a0e6e9ea9fd0f7
Jeremiah Mahler <jmmahler@gmail.com>
2014-07-05 09:27:07 -0700

    id_sysfs: simplify without ktype, remove release
    A ktype is good to use when there are many kobjects that behave the
    same.  But id_sysfs has only one and there is no need for similar types.
    Simplify by removing ktype and using __ATTR.
    
    The sysfs documentation (Documentation/kobject.txt) says a release
    method must be used.  But the example they refer to doesn't
    (samples/kobject/kobject-example.c).  Remove the release function.

--- id_jiff_foo/id_sysfs/id_sysfs.c
+++ id_jiff_foo/id_sysfs/id_sysfs.c
@@ -1,24 +1,21 @@
 
-#include <linux/fs.h>
-#include <linux/init.h>
 #include <linux/kobject.h>
-#include <linux/module.h>
-#include <linux/sysfs.h>
 #include <linux/string.h>
-
-#define MODULE_NAME "id_sysfs"
+#include <linux/sysfs.h>
+#include <linux/module.h>
+#include <linux/init.h>
 
 const char id[] = "aeda58c25c67";
 #define ID_LEN (ARRAY_SIZE(id) - 1)
 
-static ssize_t id_sysfs_show(struct kobject *kobj, struct attribute *attr,
+static ssize_t id_show(struct kobject *kobj, struct kobj_attribute *attr,
 					char *buf)
 {
 	return sprintf(buf, "%s\n", id);
 }
 
 /* return count if written id is correct, otherwise return -EINVAL */
-static ssize_t id_sysfs_store(struct kobject *kobj, struct attribute *attr,
+static ssize_t id_store(struct kobject *kobj, struct kobj_attribute *attr,
 					const char *buf, size_t count)
 {
 	/* an id with or without a new-line is OK */
@@ -35,27 +32,11 @@ static ssize_t id_sysfs_store(struct kobject *kobj, struct attribute *attr,
 	return count;  /* correct */
 }
 
-void id_sysfs_release(struct kobject *kobj)
-{
-}
-
-static const struct sysfs_ops id_sysfs_ops = {
-	.show  = id_sysfs_show,
-	.store = id_sysfs_store,
-};
-
-static struct kobj_type id_sysfs_ktype = {
-	.release   = id_sysfs_release,
-	.sysfs_ops = &id_sysfs_ops,
-};
-
-struct attribute id_sysfs_attr = {
-	.name = "id",
-	.mode = 0666,
-};
+static struct kobj_attribute id_attribute =
+	__ATTR(id, 0666, id_show, id_store);
 
 static struct attribute *attrs[] = {
-	&id_sysfs_attr,
+	&id_attribute.attr,
 	NULL
 };
 
@@ -69,11 +50,9 @@ static int __init id_sysfs_init(void)
 {
 	int ret;
 
-	kobj = kobject_create_and_add(MODULE_NAME, kernel_kobj);
+	kobj = kobject_create_and_add("id_sysfs", kernel_kobj);
 	if (!kobj)
 		return -ENOMEM;
-
-	kobj->ktype = &id_sysfs_ktype;
 
 	ret = sysfs_create_group(kobj, &attr_group);
 	if (ret)
