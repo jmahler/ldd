f38794fe74cfa4954f702e05767ddf1be5d7edb7
Jeremiah Mahler <jmmahler@gmail.com>
2014-07-05 12:23:03 -0700

    test-jiff.sh: fix tab/space usage

--- id_jiff_foo/test-jiff.sh
+++ id_jiff_foo/test-jiff.sh
@@ -4,8 +4,8 @@ if [ -z "$1" ]; then
 	cat <<-EOF
 		  usage: $0 <device>
 		    <device> := "/sys/kernel/debug/jiff_debugfs/jiffies" |
-                        "/sys/kernel/jiff_sysfs/jiffies" |
-						...
+		                "/sys/kernel/jiff_sysfs/jiffies" |
+		                ...
 	EOF
 	exit 1
 fi
