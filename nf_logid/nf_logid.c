daca7fbef9ae9d8d6d843182f0f10a7a65343c83
Jeremiah Mahler <jmmahler@gmail.com>
2014-10-08 22:48:54 -0700

    nf_logid: simplify using builtin textsearch
    Simplify nf_logid by removing the custom text search function and
    instead use the textsearch functions included with Linux
    (linux/textsearch.h).

--- nf_logid/nf_logid.c
+++ nf_logid/nf_logid.c
@@ -2,33 +2,14 @@
 #include <linux/module.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/skbuff.h>
+#include <linux/textsearch.h>
 
 static const char PATTERN[] = "FIXME";
 #define PATTERN_LEN (ARRAY_SIZE(PATTERN) - 1)
 
-/* Test whether a pattern is in a data buffer.
- * Returns: TRUE if yes, FALSE if no.
- */
-static unsigned char in_buf(const char *ptrn, const unsigned int ptrn_len,
-				const char *data, const unsigned int data_len)
-{
-	unsigned int i;
-
-	/* impossible to find pattern without enough data */
-	if (ptrn_len > data_len)
-		return 0;
-
-	/* look for the pattern */
-	for (i = 0; i < ptrn_len; i++) {
-		if (*(data + i) != *(ptrn + i))
-			break;
-	}
-	if (i == ptrn_len)
-		return 1;  /* found a match */
-
-	/* continue searching */
-	return in_buf(ptrn, ptrn_len, data + 1, data_len - 1);
-}
+static struct ts_config *search;
+static struct ts_state state;
 
 static unsigned int logid_fn(const struct nf_hook_ops *ops,
 				struct sk_buff *skb,
@@ -36,7 +17,7 @@ static unsigned int logid_fn(const struct nf_hook_ops *ops,
 				const struct net_device *out,
 				int (*okfn)(struct sk_buff *))
 {
-	if (in_buf(PATTERN, PATTERN_LEN, skb->data, skb_headlen(skb)))
+	if (UINT_MAX != skb_find_text(skb, 0, skb->len, search, &state))
 		pr_debug("pattern '%s' matched\n", PATTERN);
 
 	return NF_ACCEPT;
@@ -51,13 +32,33 @@ static struct nf_hook_ops logid_ops = {
 
 static int __init init_logid(void)
 {
-	return nf_register_hook(&logid_ops);
+	int ret = 0;
+
+	search = textsearch_prepare("kmp", PATTERN, PATTERN_LEN, GFP_KERNEL,
+							TS_AUTOLOAD);
+	if (IS_ERR(search)) {
+		ret = PTR_ERR(search);
+		goto textsearch_prepare_failed;
+	}
+
+	ret = nf_register_hook(&logid_ops);
+	if (ret)
+		goto nf_register_hook_failed;
+
+	return ret;
+
+nf_register_hook_failed:
+	textsearch_destroy(search);
+textsearch_prepare_failed:
+
+	return ret;
 }
 module_init(init_logid);
 
 static void __exit exit_logid(void)
 {
 	nf_unregister_hook(&logid_ops);
+	textsearch_destroy(search);
 }
 module_exit(exit_logid);
 
